9eb3ef1c521ba42978081c14aaae382298b0a9ae
RobertHubbard <Rob.hubbard0815@gmail.com>
2017-10-17 23:16:11 -0500

    Update MainApp.java

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,6 +2,6 @@
 public class MainApp {
     public static void main (String[] args){
         System.out.println("Hello Git Hub");
-        System.out.println("UGHHH");
+        System.out.println("Woohoo");
     }
 }
