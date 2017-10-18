fcd44c9656b4108b33cc892c5cf47b0d23efe33d
RobertHubbard <Rob.hubbard0815@gmail.com>
2017-10-17 22:51:06 -0500

    This is a test

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,7 @@
 
 public class MainApp {
     public static void main (String[] args){
-        System.out.print("Hello Git Hub");
+        System.out.println("Hello Git Hub");
+        System.out.println("Bad change test");
     }
 }
