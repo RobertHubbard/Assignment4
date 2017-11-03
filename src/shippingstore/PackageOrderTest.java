6325e2f12f4b1728be72c0c0244c97615ba6b5f7
RobertHubbard <Rob.hubbard0815@gmail.com>
2017-11-02 19:37:24 -0500

    Test2

--- src/shippingstore/PackageOrderTest.java
+++
@@ -1,8 +0,0 @@
-package shippingstore;
-
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.*;
-
-class PackageOrderTest {
-
-}
--- src/shippingstore/ShippingStoreTest.java
+++ src/shippingstore/ShippingStoreTest.java
@@ -2,6 +2,25 @@ package shippingstore;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import java.io.IOException;
+
 class ShippingStoreTest {
+    public static ShippingStore shippingstore = null;
+    @BeforeClass
+    public static void createEnvironment() throws IOException {
+        shippingstore = new ShippingStore();
+        System.out.println("ENVIRONMENT CREATED");
+    }
+    @AfterClass
+    public static void clearEnvironment(){
+        shippingstore = null;
+        System.out.println("ENVIRONMENT CLEARED");
+    }
+
+
+
 
 }
